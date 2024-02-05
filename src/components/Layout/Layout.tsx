a8a535be7ab0b0aaadac4a60b9c7b75392be7a1f
yechan <kevin2695@naver.com>
2024-02-05 23:30:26 +0900

    [240205] layout 추가

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div>
+      {/* <NavigationBar /> */}
+      11
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
